d37469b60efcd4152996a9c6144e1e80ec2af4e8
genkjames <32821241+genkjames@users.noreply.github.com>
2018-06-29 17:41:06 -0400

    retrieve weather data for the cities

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import Landing from '../components/Landing';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+
+    }
+  }
+
+  fetchWeatherData() {
+    fetch(`http://api.openweathermap.org/data/2.5/group?id=1609350,2643743,6455259,1880252,5128638&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    this.fetchWeatherData();
+  }
+
   render() {
     return (
       <div className="App">
